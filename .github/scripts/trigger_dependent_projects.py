5c8f08e5c30cb034717de3fdd57190b3265afeb4
OMGerEDU <73582816+OMGerEDU@users.noreply.github.com>
2023-03-28 14:30:42 +0300

    Update trigger_dependent_projects.py

--- .github/scripts/trigger_dependent_projects.py
+++ .github/scripts/trigger_dependent_projects.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from github import Github
 
 def main():
@@ -8,9 +9,21 @@ def main():
     search_query = 'topic:autoversioning-dependent'
     repositories = github.search_repositories(query=search_query)
 
+    headers = {
+        'Accept': 'application/vnd.github.everest-preview+json',
+        'Authorization': f'token {gh_token}'
+    }
+
     for repo in repositories:
         print(f'Triggering update for {repo.full_name}')
-        repo.dispatch_event('update-versioning-scripts')
+        url = f'https://api.github.com/repos/{repo.full_name}/dispatches'
+        payload = {'event_type': 'update-versioning-scripts'}
+        response = requests.post(url, headers=headers, json=payload)
+
+        if response.status_code == 204:
+            print(f'Successfully triggered update for {repo.full_name}')
+        else:
+            print(f'Error triggering update for {repo.full_name}. Status code: {response.status_code}')
 
 if __name__ == "__main__":
     main()
